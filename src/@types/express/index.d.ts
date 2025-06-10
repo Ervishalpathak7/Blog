dc45cab83af03b57fe49f28780f1ea5a55a9bd88
Vishal Pathak <vishalpathak.er@gmail.com>
2025-06-10 15:28:10 +0530

    add custom types

---
+++ src/@types/express/index.d.ts
@@ -0,0 +1,13 @@
+// Project: @types/express
+import * as express from 'express';
+
+// import types
+import { Types } from 'mongoose';
+
+declare global {
+  namespace Express {
+    interface Request {
+      userId?: Types.ObjectId;
+    }
+  }
+}
