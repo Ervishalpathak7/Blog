30ceb32aa88758f8e50c32198960ad0a96809bc8
Vishal Pathak <vishalpathak.er@gmail.com>
2025-06-10 13:35:34 +0530

    add register controller

--- src/controllers/v1/auth/register.ts
+++ src/controllers/v1/auth/register.ts
@@ -2,8 +2,10 @@ import logger from '@/lib/winston';
 import type { Request, Response } from 'express';
 import user from '@/models/user';
 import type { IUser } from '@/models/user';
+import refreshToken from '@/models/refreshToken';
 import { genUsername } from '@/utils';
 import { generateAccessToken, generateRefreshToken } from '@/lib/jwt';
+import config from '@/config';
 
 // Define the type for user data to be registered
 type UserData = Pick<IUser, 'email' | 'password' | 'role'>;
@@ -20,6 +22,20 @@ const registerController = async (
   try {
     // Extract user data from the request body
     const { email, password, role } = req.body as UserData;
+
+    if(role === 'admin' && !config.WHITELIST_EMAILS.includes(email)) {
+      logger.warn(`Unauthorized admin registration attempt`, {
+        email,
+      });
+      res.status(403).json({
+        status: 'error',
+        message: 'Unauthorized to register as admin',
+        timestamp: new Date().toISOString(),
+      });
+      return
+    }
+
+
     // Generate a random username
     const username = genUsername();
 
@@ -32,17 +48,27 @@ const registerController = async (
     });
 
     // generate access token and refresh token for new user
-    const accessToken = generateAccessToken(newUser._id);
-    const refreshToken = generateRefreshToken(newUser._id);
+    const newAccessToken = generateAccessToken(newUser._id);
+    const newRefreshToken = generateRefreshToken(newUser._id);
+
+    // store the refresh token in the database
+    await refreshToken.create({
+      token: newRefreshToken,
+      userId: newUser._id,
+    });
+
+    logger.info(`New Refresh Token created for user`, {
+      userId: newUser._id,
+    });
 
     // set the refresh token in a secure, httpOnly cookie
-    res.cookie('refreshToken', refreshToken, {
+    res.cookie('refreshToken', newRefreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
       sameSite: 'strict', // Prevent CSRF attacks
       maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
     });
-
+ 
     res.status(200).json({
       status: 'ok',
       message: 'User Registered Successfully',
@@ -52,7 +78,7 @@ const registerController = async (
         username: newUser.username,
         role: newUser.role,
       },
-      accessToken,
+      newAccessToken,
       timestamp: new Date().toISOString(),
     });
 
@@ -63,11 +89,10 @@ const registerController = async (
       role: newUser.role,
     });
   } catch (error) {
-    logger.error(`Error in registerController: ${error}`);
+    logger.error(`Error in register : ${error}`);
     res.status(500).json({
       status: 'error',
       message: error instanceof Error ? error.message : 'Internal Server Error',
-      timestamp: new Date().toISOString(),
     });
   }
 };
